1df8cfe96942be0bfe30a91aaf505e7890612e85
Abdullah <abdullahelagami55@gmail.com>
2024-02-16 04:55:10 +0200

    Develop/Remove unused imports

--- maping/src/main/java/com/example/maping/repository/JobApplicationRepo.java
+++ maping/src/main/java/com/example/maping/repository/JobApplicationRepo.java
@@ -1,12 +1,8 @@
 package com.example.maping.repository;
 
-import com.example.maping.entity.Application;
 import com.example.maping.entity.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface JobApplicationRepo extends JpaRepository<JobApplication, Long> {
