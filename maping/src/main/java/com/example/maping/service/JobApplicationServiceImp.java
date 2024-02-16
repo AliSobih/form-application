68181d52dddcb633fbcf30e05212aaf6c881562e
Abdullah <abdullahelagami55@gmail.com>
2024-02-16 04:49:55 +0200

    Develop/Use the ApplicationBaseService to save

--- maping/src/main/java/com/example/maping/service/JobApplicationServiceImp.java
+++ maping/src/main/java/com/example/maping/service/JobApplicationServiceImp.java
@@ -1,55 +1,36 @@
 package com.example.maping.service;
 
-import com.example.maping.dto.ApplicationRequest;
 import com.example.maping.entity.JobApplication;
-import com.example.maping.entity.Review;
-import com.example.maping.entity.Skill;
-import com.example.maping.enumPackage.DepartmentRole;
 import com.example.maping.repository.JobApplicationRepo;
 import com.example.maping.repository.ReviewRepo;
-import com.example.maping.repository.SkillRepo;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.EnumSet;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-public class JobApplicationServiceImp implements ApplicationService<JobApplication> {
+public class JobApplicationServiceImp extends ApplicationBaseService<JobApplication> implements ApplicationService<JobApplication> {
     private final JobApplicationRepo jobApplicationRepo;
-    private final ReviewRepo reviewRepo;
-    private final SkillRepo skillRepo;
-    @Override
-    public void saveApplication(ApplicationRequest request) {
-        JobApplication application = new JobApplication(
-                request.firstName,
-                request.lastName,
-                request.email,
-                request.description
-        );
 
-        List<Skill> skillList = skillRepo.findAllById(request.skillList);
-        application.setSkills(skillList);
-        jobApplicationRepo.save(application);
-
-        List<DepartmentRole> roleList = EnumSet.allOf(DepartmentRole.class).stream().toList();
-        for (DepartmentRole role : roleList) {
-            Review review = new Review(
-                    role,
-                    application
-            );
-            reviewRepo.save(review);
-        }
+    public JobApplicationServiceImp(ReviewRepo reviewRepo, JobApplicationRepo jobApplicationRepo) {
+        super(reviewRepo);
+        this.jobApplicationRepo = jobApplicationRepo;
     }
 
     @Override
-    public List<JobApplication> findAllApplications() {
+    @Transactional
+    public void save(JobApplication application) {
+        super.save(application);
+        jobApplicationRepo.save(application);
+    }
+
+    @Override
+    public List<JobApplication> findAll() {
         return jobApplicationRepo.findAll();
     }
 
     @Override
-    public JobApplication findApplicationById(Long id) {
+    public JobApplication findById(Long id) {
         return jobApplicationRepo.findById(id).orElse(null);
     }
 }
